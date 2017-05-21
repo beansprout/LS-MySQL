9c854842dbacb5dd08453607fd09abbcf6983210
Christine Gierer <beansprout@users.noreply.github.com>
2017-05-20 21:12:28 -0400

    fixed incorrect table name, added get reqs /, /todos, /todos/:id, added post /todos

--- expressserver/server.js
+++ expressserver/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const config = require('../config.json');
+// const config = require('../config.json');
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+// const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+const sequelize = new Sequelize('mysql://christine:password@localhost:3306/todos');
 
 // set up the express app
 const app = express();
@@ -12,7 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // set up model
-const Tasks = sequelize.define('todo_list', {
+const Tasks = sequelize.define('todos', {
   todo: {type: Sequelize.STRING},
   complete: {type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false},
 });
@@ -20,15 +22,32 @@ const Tasks = sequelize.define('todo_list', {
 // the following commented out is for creating a first record in the newly defined table
 // Tasks.sync().then(() => {
 //   return Tasks.create({
-//     task: 'finish project'});
+//     todo: 'finish project'});
 // });
 
 // home route - check if express server working
 app.get('/', (req, res) => res.status(200).send({message: 'Welcome to the Restaurant at the End of the Universe.'}));
 
-app.get('/tasks', (req, res) => {
+// return an array of all todos (todos:todos i.e. db:table)
+app.get('/todos', (req, res) => {
   Tasks.findAll().then((todos) => {
     res.send(todos);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  Tasks.findById(id).then((todos) => {
+    res.send(todos);
+  });
+});
+
+app.post('/todos', (req, res) => {
+  Tasks.build({
+    todo: req.body.todo,
+    complete: req.body.complete,
+  }).save(() => {
+    res.send('todo saved');
   });
 });
 
